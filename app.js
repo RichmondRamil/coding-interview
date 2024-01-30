24cdcc3df20388192c92c98de1f940f7b788bdfc
kerbethecoder <krbycnts@gmail.com>
2024-01-30 14:17:16 +0800

    finished the first given problem

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 // app.js
-const express = require("express");
+const express = require('express');
 const app = express();
-const axios = require("axios");
+const axios = require('axios');
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
 });
-var animalsArray = ["Elephant", "Eagle", "Dragonfly"];
 
-// Can you categorize the animalsArray into the following categories?
+var animalsArray = ['Elephant', 'Eagle', 'Dragonfly'];
+// 1. I need to define the desired structure
+const animals = {
+  Mammals: [],
+  Birds: [],
+  Insects: [],
+};
 
+// 2. Checking where does each of the animals in the given array falls into
+animalsArray.forEach((animal) => {
+  if (animal.includes('Elephant')) {
+    animals.Mammals.push(animal);
+  } else if (animal.includes('Eagle')) {
+    animals.Birds.push(animal);
+  } else if (animal.includes('Dragonfly')) {
+    animals.Insects.push(animal);
+  }
+});
+
+// 3. See if I did the right thing
+console.log(animals);
 // Desired Output
 // {
-//     Mammals: [ 'Elephant' ],
-//     Birds: [ 'Eagle' ],
-//     Insects: [ 'Dragonfly' ]
-// }
-
-//
+//   Mammals: ['Elephant'],
+//   Birds: ['Eagle'],
+//   Insects: ['Dragonfly'],
+// };
 
 // After you get the design output create an API call to this API endpoint
 // https://api.api-ninjas.com/v1/animals
